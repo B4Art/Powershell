7fb339b247edf4c2a3bb467b794e9e34904201c4
Bart van den Donk <bvandendonk@de-alliantie.nl>
2021-01-18 18:04:17 +0100

    Solved

--- Get-Between.ps1
+++ Get-Between.ps1
@@ -3,9 +3,6 @@ Param (
 	[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
 	$List,
 	[Parameter(Mandatory)]
-	[ArgumentCompleter( {
-		($List | Get-Member -MemberType NoteProperty).Name
-	} )]
 	[string] $NotePropertyName,
 	[switch] $XorNotBetween
 )
@@ -20,21 +17,23 @@ function Get-Between{
 		[switch] $XorNotBetween
 	)
 	Begin {
-		$min = ($List.$NotePropertyName | Measure-Object -Minimum).Minimum;
+		$min = ($List.$NotePropertyName | Measure-Object -Minimum).Minimum
 		$max = ($List.$NotePropertyName | Measure-Object -Maximum).Maximum
 	}
 	Process {
 		If ($XorNotBetween) {
-			$List.Where({$_.$NotePropertyName -in ($min, $max)})
+			Write-Output $List.Where({$_.$NotePropertyName -in ($min, $max)})
 		} else {
-			$List.Where({$_.$NotePropertyName -notin ($min, $max)})
+			Write-Output $List.Where({$_.$NotePropertyName -notin ($min, $max)})
 		}
 	}
 	End {
 	}
 }
 
+$scriptblock = {
+	param($commandName, $parameterName, $stringMatch)
+	($List | Get-Member -MemberType NoteProperty).Name
+}
 
-Get-Between $List $NotePropertyName
-
-# [System.Management.Automation.Language.Parser]::ParseInput($MyInvocation.MyCommand.ScriptContents, [ref]$null, [ref]$null)
+Register-ArgumentCompleter -CommandName Get-Between -ParameterName NotePropertyName -ScriptBlock $scriptBlock
