19a46eb2b0b2e11349400f87baaa20994f747a4f
Bart van den Donk <bvandendonk@de-alliantie.nl>
2021-01-22 16:08:54 +0100

    Eindelijk

--- Get-Between.ps1
+++ Get-Between.ps1
@@ -1,3 +1,4 @@
+<#
 [CmdletBinding()]
 Param (
 	[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
@@ -6,34 +7,51 @@ Param (
 	[string] $NotePropertyName,
 	[switch] $XorNotBetween
 )
-
+#>
 function Get-Between{
 	[CmdletBinding()]
 	Param (
-		[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
-		$List,
-		[Parameter(Mandatory)]
+		[Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+		[System.Object]$List,
+		[Parameter(Mandatory, Position = 1)]
+		# [ArgumentCompletions({([ref]$List | Get-Member -MemberType NoteProperty).Name})]
 		[string] $NotePropertyName,
 		[switch] $XorNotBetween
 	)
+	
 	Begin {
 		$min = ($List.$NotePropertyName | Measure-Object -Minimum).Minimum
 		$max = ($List.$NotePropertyName | Measure-Object -Maximum).Maximum
-	}
-	Process {
+	} Process {
 		If ($XorNotBetween) {
 			Write-Output $List.Where({$_.$NotePropertyName -in ($min, $max)})
 		} else {
 			Write-Output $List.Where({$_.$NotePropertyName -notin ($min, $max)})
 		}
-	}
-	End {
+	} End {
 	}
 }
 
-$scriptblock = {
-	param($commandName, $parameterName, $stringMatch)
-	($List | Get-Member -MemberType NoteProperty).Name
+$GetNotePropertyNameCompleter = {
+	param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+	Return ($fakeBoundParameter["List"] | Get-Member -MemberType NoteProperty | ForEach-Object { $_.Name })
 }
 
-Register-ArgumentCompleter -CommandName Get-Between -ParameterName NotePropertyName -ScriptBlock $scriptBlock
+Register-ArgumentCompleter -CommandName Get-Between -ParameterName NotePropertyName -ScriptBlock $GetNotePropertyNameCompleter
+
+<#
+$ScriptBlock = [scriptblock]::Create( {
+		param ( $CommandName,
+			$ParameterName,
+			$WordToComplete,
+			$CommandAst,
+			$FakeBoundParameters )
+ 
+		$NotePropertyNames = $List | Get-Member -MemberType NoteProperty
+		$NotePropertyNames.Name | ForEach-Object { $_ }
+	})
+ 
+Register-ArgumentCompleter -CommandName Get-Between -ParameterName NotePropertyName -ScriptBlock $ScriptBlock
+
+<##>
+#>
