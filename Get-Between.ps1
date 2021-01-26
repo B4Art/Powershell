9f521368c08f6ed7bf407426f3f10a546beb00f3
Bart van den Donk <bvandendonk@de-alliantie.nl>
2021-01-26 10:12:11 +0100

    Not working

--- Get-Between.ps1
+++ Get-Between.ps1
@@ -14,10 +14,16 @@ function Get-Between{
 		[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
 		[System.Object]$List,
 		[Parameter(Mandatory, Position = 1)]
+		#
 		[ValidateScript( {
 			param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-			($fakeBoundParameter["List"] | Get-Member -MemberType NoteProperty).Where( { $_.Name -like "$wordToComplete*" }).ForEach( { [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name) }) 
-		}) ]
+			($fakeBoundParameter["List"] | Get-Member -MemberType NoteProperty).Where( {
+				$_.Name -like "$wordToComplete*"
+			} ).ForEach( {
+				[System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+			} ) 
+		} ) ]
+		#>
 		[string] $NotePropertyName,
 		[Parameter(Position = 2)]
 		[switch] $XorNotBetween
@@ -38,9 +44,24 @@ function Get-Between{
 <#
 $GetNotePropertyNameCompleter = {
 	param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-	($fakeBoundParameter["List"] | Get-Member -MemberType NoteProperty).Where( { $_.Name -like "$wordToComplete*" }).ForEach( { [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name) })
+	( $fakeBoundParameter["List"] | Get-Member -MemberType NoteProperty ).Where( {
+		$_.Name -like "$wordToComplete*"
+	} ).ForEach( {
+		 [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+	} )
 }
 
 Register-ArgumentCompleter -CommandName Get-Between -ParameterName NotePropertyName -ScriptBlock $GetNotePropertyNameCompleter
 # Register-ArgumentCompleter -ParameterName NotePropertyName -ScriptBlock $GetNotePropertyNameCompleter
 #>
+
+$h = Get-Content '.\examples\import_from_openam_response.json' | ConvertFrom-Json | Select-Object id, username, email, @{l = 'customerNumber'; e = { [string]$e = $_.attributes.customerNumber; $e = $e.Trim(); Try { [System.Text.Encoding]::UTF8.GetString( [System.Convert]::FromBase64String( $e ) ) } catch { $e } } }
+
+$e = $h |
+    Group-Object -Property customerNumber -AsHashTable -AsString
+$f = $h |
+    Select-Object *, @{l = 'intDubbel'; e = {$e[$_.customerNumber].Count}}
+
+($f | Get-Member -MemberType NoteProperty).Name
+
+# Get-Between -List $f -NotePropertyName 
